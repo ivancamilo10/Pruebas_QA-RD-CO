2f6951f9c42a00da0f4aae05f108ff7ae7e5ac97
Ali Ortiz Povea <alortiz@gbm.net>
2025-10-02 17:29:53 -0500

    creacion de ecommerce basico para pruebas

--- AliOrtiz/proyectocypress/cypress/e2e/login.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Mi primera prueba', () => {
-  it('Visita la página de ejemplo', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('type').click()
-    cy.url().should('include', '/commands/actions')
-  })
-})
